0e224dacccf63e8464c076890a2015b1148586b5
seol <tjftmd7@gmail.com>
2024-05-20 01:07:02 +0900

    add files

---
+++ study/src/main/java/UMC/study/domain/Review.java
@@ -0,0 +1,25 @@
+package UMC.study.domain;
+
+import UMC.study.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
+
+
